a41bcc860dd0a0e22ae7566a6c3698bb5f6a1db2
ellamvdh <fungirl2050@gmail.com>
2026-05-12 15:43:18 +0200

    fireball tower works

---
+++ Tower Defense Finland/Assets/FireAreaLvl1.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class FireAreaLvl1 : MonoBehaviour
+{
+    public GameObject EnemyToDamage;
+    private IEnemy enemy;
+    public float timer;
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        
+        if(EnemyToDamage == null && enemy != null)
+        {
+            enemy = null;
+        }else if(EnemyToDamage != null )
+        {
+            if(enemy == null)
+            {
+                enemy = EnemyToDamage.GetComponent<IEnemy>();
+            }
+            if(timer > 1)
+            {
+                Debug.Log("Damage Enemy");
+                enemy.Kill(10);
+                timer = 0;
+            }
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        EnemyToDamage = other.gameObject;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        EnemyToDamage = null;
+    }
+}
