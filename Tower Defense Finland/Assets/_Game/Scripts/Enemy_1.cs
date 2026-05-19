93503e7ef2d5182abfa1ec2925548b7518a6b98a
Dooruss <doorusdebeijer@gmail.com>
2026-05-19 11:19:10 +0200

    Chore: Comments in enemy 1

--- Tower Defense Finland/Assets/_Game/Scripts/Enemy_1.cs
+++ Tower Defense Finland/Assets/_Game/Scripts/Enemy_1.cs
@@ -16,24 +16,25 @@ public class Enemy_1 : Entity, IEnemy
         CheckDeath();
         if (cartScript.m_Position == 1 && !IsParalelTrack)
         {
+            // Als het geen parelel is (gwn 1 lijn) en de cart is op positie 1 (eind) dan gaat de cart dood en valt het aan op de toren
             OnDeath(true);
         }
-        else if (cartScript.m_Position == 1 && IsParalelTrack)
+        else if (cartScript.m_Position == 1 && IsParalelTrack) //als het wel paralel is
         {
-            if (cartScript.m_Path == AllPaths[0])
+            if (cartScript.m_Path == AllPaths[0]) // Als het bij het einde van de eerste 'lijn' is
             {
-                int RandomNumber = Random.Range(1, 3);
+                int RandomNumber = Random.Range(1, 3); // kiest links of rechts
                 cartScript.m_Position = 0;
-                cartScript.m_Path = AllPaths[RandomNumber];
+                cartScript.m_Path = AllPaths[RandomNumber]; // gaat naar 1 of 2
             }
-            else if (cartScript.m_Path == AllPaths[1] || cartScript.m_Path == AllPaths[2])
+            else if (cartScript.m_Path == AllPaths[1] || cartScript.m_Path == AllPaths[2]) // aan het einde van links of rechts
             {
                 cartScript.m_Position = 0;
-                cartScript.m_Path = AllPaths[3];
+                cartScript.m_Path = AllPaths[3]; // ga naar de laatste lijn
             }
             else
             {
-                OnDeath(true);
+                OnDeath(true); // als het dan bij het einde van de laatste lijn is , gaat het dood en valt het aan op de toren
             }
         }
     }
