bd69ce3f059e207c7d034bf4c444784e942be93a
ellamvdh <fungirl2050@gmail.com>
2026-06-24 16:12:52 +0200

    busy w money system

--- Tower Defense Finland/Assets/Tiling scripts/MoneyManager.cs
+++ Tower Defense Finland/Assets/Tiling scripts/MoneyManager.cs
@@ -1,7 +1,22 @@
+using TMPro;
 using UnityEngine;
 
 public class MoneyManager : MonoBehaviour
 {
     public int Money;
-    
+
+    public int ThorTowers = 0;
+    public int FireTowers = 0;
+    public int AOEtowers = 0;
+
+    [SerializeField] TextMeshProUGUI ThorTowerText;
+    [SerializeField] TextMeshProUGUI FireTowerText;
+    [SerializeField] TextMeshProUGUI AOEtowText;
+
+    private void Update()
+    {
+        ThorTowerText.text = ThorTowers.ToString();
+        FireTowerText.text = FireTowers.ToString();
+        AOEtowText.text = AOEtowers.ToString();
+    }
 }
