f7b8efeb4de9629745a706738a66280b1e44373b
ellamvdh <fungirl2050@gmail.com>
2026-06-30 11:37:19 +0200

    ok you can upgrade towers nowww

---
+++ Tower Defense Finland/Assets/Tiling scripts/ThorUpgrade.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class ThorUpgrade : MonoBehaviour
+{
+    MoneyManager moneyManager;
+    int UpgradePrice = 300;
+    [SerializeField] GameObject UpgradeShit;
+    [SerializeField] int Level = 0;
+    void Start()
+    {
+        moneyManager = GameObject.Find("GameManager").GetComponent<MoneyManager>();
+    }
+
+    void Update()
+    {
+        if(moneyManager.Money >= UpgradePrice && Level < 3)
+        {
+            UpgradeShit.SetActive(true);
+        }
+        else
+        {
+            UpgradeShit.SetActive(false);
+        }
+    }
+
+    public void UpgradeThor()
+    {
+        moneyManager.Money -= UpgradePrice;
+        moneyManager.ThorTowerLiftetime += 5f;
+        Debug.Log("Thor tower lifetime is now" + moneyManager.ThorTowerLiftetime);
+        Level++;
+    }
+
+    public void UpgradeFire()
+    {
+        moneyManager.Money -= UpgradePrice;
+        moneyManager.FireTowerLifeTime += 5;
+        Debug.Log("Fire tower lifetime is now" + moneyManager.FireTowerLifeTime);
+        Level++;
+    }
+
+    public void UpgradeAOE()
+    {
+        moneyManager.Money -= UpgradePrice;
+        moneyManager.FireTowerLifeTime += 5;
+        Debug.Log("AEO tower lifetime is now " + moneyManager.AOETowerLifeTime);
+        Level++;
+    }
+}
